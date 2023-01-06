632bbdd8fbced3753f76e45259950163d9487d4a
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-06 12:02:58 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -2,8 +2,8 @@ data "aws_ami" "redhat" {
   most_recent = true
 
   filter {
-    name = "name"
-    ami  = ["ami-08e637cea2f053dfa"]
+    name   = "name"
+    values = ["ami-08e637cea2f053dfa"]
   }
 
 }
